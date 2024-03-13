6789a89c185b9315f640b6c0c1374650e83c3c23
DPAVLOV <Dmytro.Pavlov@davita.com>
2024-03-13 15:51:02 -0600

    second commit

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ from flask import Flask, request
 # cos_sim = lambda a: norm(a)
 
 app = Flask(__name__)
-model = AutoModel.from_pretrained('./model/jina-embeddings-v2-base-en', trust_remote_code=True)
+model = AutoModel.from_pretrained('./model', trust_remote_code=True)
 
 
 @app.route('/get-embedding', methods=['POST'])
 def add_income():
     text = request.get_json()['text']
     value = array('f', model.encode([text]).tolist()[0])
-    print(text)
-    print(value.__len__())
-    print(value.tolist())
     return value.tolist(), 200
 
 app.run(debug=True, host='localhost', port=8080)
